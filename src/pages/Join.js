def1857b1239607554db571cbb53c6ff5297f442
g0yujin <dbwbsqhd016@gmail.com>
2024-05-17 01:35:59 +0900

    DESIGN implement Join.js

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,57 +1,66 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Join.css'; // Screen 컴포넌트의 스타일을 포함합니다.
 
 const Join = () => {
+
+    
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+    navigate(-1); // 이전 페이지로 이동
+    };
+
     return (
         <div className="div-join">
-            <div className="title-join">
-                <div className="text-wrapper">회원 가입</div>
-                <img className="vector" alt="Vector" src="vector/back.svg" />
+        <div className="title-join">
+            <div className="join-title">회원 가입</div>
+            <img className="back-join" alt="Vector" src="vector/back.svg" onClick={handleBack}/>
+        </div>
+        <div className="div-form">
+            <div className="form-join">
+            <img className="img-profile" alt="Profile" src="images/profile.png" />
+            <img className="rect-form" alt="Rectangle" src="vector/rect-join.svg" />
+            <div className="div-password">
+                <input className="rect-input1" type="password" placeholder=" " />
+                <p className="title-password">
+                <span className="span">비밀번호</span>
+                <span className="star">*</span>
+                </p>
+                <p className="notice-password">8-20자 이내 , 숫자 , 영어</p>
             </div>
-            
-            <div className="overlap">
-                <img className="profile" alt="Profile" src="images/profile.png" />
-                <img className="rectangle" alt="Rectangle" src="vector/rect-join.svg" />
-                <p className="div">8-20자 이내, 숫자, 영어</p>
-                <img className="img" alt="Rectangle" src="rectangle-83.svg" />
-                <p className="p">
-                    <span className="span">비밀번호</span>
-                    <span className="text-wrapper-2">*</span>
+            <div className="div-password2">
+                <input className="rect-input2" type="password" placeholder=" " />
+                <div className="title-password2">비밀번호 확인</div>
+            </div>
+            <div className="div-button-ok">
+                <button className="button-ok">확인</button>
+            </div>
+            <div className="div-id">
+                <p className="title">
+                <span className="span">아이디</span>
+                <span className="star">*</span>
                 </p>
-                <img className="rectangle-2" alt="Rectangle" src="rectangle-84.svg" />
-                <div className="text-wrapper-3">비밀번호 확인</div>
-                <div className="view">
-                    <div className="overlap-group">
-                        <div className="text-wrapper-4">확인</div>
-                    </div>
+                <input className="rect-input3" type="text" placeholder=" " />
+                <div className="div-wrapper">
+                    <button className="button-dupli">중복확인</button>
                 </div>
-                <p className="div-2">
-                    <span className="span">아이디</span>
-                    <span className="text-wrapper-2">*</span>
+                <div className="notice-id">이미 사용중인 아이디입니다.</div>
+            </div>
+            <div className="div-nickname">
+                <input className="rect-input4" type="text" placeholder=" " />
+                <p className="title">
+                <span className="span">닉네임</span>
+                <span className="star">*</span>
                 </p>
-                <div className="rectangle-3" />
-                <div className="text-wrapper-5">BUBBLE</div>
-                <div className="overlap-wrapper">
-                    <div className="div-wrapper">
-                        <div className="text-wrapper-6">중복확인</div>
-                    </div>
+                <div className="div-button-dupli2">
+                    <button className="button-dupli">중복확인</button>
                 </div>
-                <div className="text-wrapper-7">이미 사용중인 아이디입니다.</div>
-                <img className="rectangle-4" alt="Rectangle" src="rectangle-82.svg" />
-                <p className="div-3">
-                    <span className="span">닉네임</span>
-                    <span className="text-wrapper-2">*</span>
-                </p>
-                <div className="overlap-group-wrapper">
-                    <div className="div-wrapper">
-                        <div className="text-wrapper-6">중복확인</div>
-                    </div>
-                </div>
-                <div className="text-wrapper-8">사용 가능한 닉네임입니다.</div>
+                <div className="notice-nickname">사용 가능한 닉네임입니다.</div>
+            </div>
             </div>
         </div>
-       
+        </div>
     );
-};
-
-export default Join;
+    };
+    export default Join;
