44fc01f3da57c423b06953d3a26dc26faa362941
g0yujin <dbwbsqhd016@gmail.com>
2024-05-18 01:43:10 +0900

    DELETE : test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
-import { useNavigate } from 'react-router-dom';
 import "../styles/Header.css";
 
 const Header = () => {
-  const navigate = useNavigate();
 
   const logoClick = () => {
     // container 클릭 시 실행할 코드
@@ -21,10 +19,7 @@ const Header = () => {
     // icon 클릭 시 실행할 코드
   };
 
-  const goToJoin = () => {
-    navigate('/join'); // '/join' 경로로 이동
-  };
-
+    
   return (
     <div className="div-header">
       <div className="div-logo" onClick={logoClick}>
@@ -34,14 +29,12 @@ const Header = () => {
         <div className="logout">logout</div>
       </div>
       <div className="div-search">
-        <input type="text" className="search-input" placeholder="Search..." />
+      <input type="text" className="search-input" placeholder="Search..."/>
         <img className="icon-search" alt="" src="images/search-icon.png" onClick={searchIconClick} />
       </div>
       <img className="icon-user" alt="" src="images/user-icon.png" id="icon1" onClick={userIconClick} />
       <img className="line-header" alt="" src="vector/line-header.svg" />
-      <button className="join-button" onClick={goToJoin}>Join</button>
     </div>
   );
 };
-
 export default Header;
