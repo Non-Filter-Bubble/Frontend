b72a40eff845a9cb703d858d3dcb9239035731b5
g0yujin <dbwbsqhd016@gmail.com>
2024-05-23 19:50:39 +0900

    DESIGN : sort and adjust subtitles

--- src/pages/BookPost.js
+++ src/pages/BookPost.js
@@ -5,47 +5,44 @@ import '../styles/BookPost.css';
 const BookPost = () => {
   return (
     <div className="div-bookpost">
-          <div className="group">
-            <div className="text-wrapper">북서랍 등록</div>
+          <div className="div-bookpost-title">
+            <div className="bookpost-title">북서랍 등록</div>
             <img className="back-book" alt="" src="/vector/back.svg" />
           </div>
           <img className="line-bookpost" alt="" src="/vector/line-book.svg" />
-          <div className="group-2">
-            <img className="rectangle" alt="" src="rectangle-82.svg" />
-            <p className="p">
-              <span className="span">저자</span>
-              <span className="text-wrapper-2">*</span>
-              <span className="span">&nbsp;</span>
+          <div className="group-author">
+            <div className="rect-author"/>
+            <p className="title-author">
+              <span className="subtitle">저자</span>
+              <span className="star">*</span>
             </p>
           </div>
-          <div className="group-3">
-            <img className="img" alt="" src="rectangle-83.svg" />
-            <p className="div-2">
-              <span className="span">출판사</span>
-              <span className="text-wrapper-2">*</span>
+          <div className="group-company">
+          <div className="rect-company"/>
+            <p className="title-company">
+              <span className="subtitle">출판사</span>
+              <span className="star">*</span>
             </p>
           </div>
-          <div className="group-4">
-            <p className="div-3">
-              <span className="span">도서명</span>
-              <span className="text-wrapper-2">*</span>
+          <div className="group-bookname">
+            <p className="title-bookname">
+              <span className="subtitle">도서명</span>
+              <span className="star">*</span>
             </p>
-            <div className="rectangle-2" />
+            <div className="rect-bookname" />
           </div>
-          <div className="overlap-group-wrapper">
-            <div className="overlap-group">
-              <img className="rectangle-3" alt="" src="rectangle-84.svg" />
-              <div className="text-wrapper-3">독서 후기</div>
+          <div className="div-review">
+              <div className="rect-review"/>
+              <div className="subtitle-review">독서 후기</div>
+          </div>
+          <div className="div-searchbook-btn">
+            <div className="searchbook-btn">
+              <div className="searchbook">도서 검색</div>
             </div>
           </div>
-          <div className="overlap-wrapper">
-            <div className="overlap-2">
-              <div className="text-wrapper-4">도서 검색</div>
-            </div>
-          </div>
-          <div className="view-2">
-            <div className="overlap-3">
-              <div className="text-wrapper-5">등록</div>
+          <div className="div-submit">
+            <div className="subit-btn">
+              <div className="submit">등록</div>
             </div>
           </div>
           <div className="div-good">
@@ -54,17 +51,15 @@ const BookPost = () => {
           <div className="div-bad">
             <img className="btn-bad" alt="" src="/images/empty-bad-btn.png" />
           </div>
-          <div className="group-5">
-            <div className="text-wrapper-6">50자 이내</div>
+          <div className="div-one-line">
+            <div className="one-line-notice">50자 이내</div>
             <div className="overlap-4">
-              <div className="group-6">
-                <img className="rectangle-4" alt="" src="image.svg" />
-                <p className="div-4">
-                  <span className="span">한줄평</span>
-                  <span className="text-wrapper-2">*</span>
+                <div className="rect-one-line" />
+                <p className="title-one-line">
+                  <span className="subtitle">한줄평</span>
+                  <span className="star">*</span>
                 </p>
-              </div>
-              <div className="text-wrapper-7">다른 사람에게 책을 소개해주세요.</div>
+              <div className="one-line-notice-2">다른 사람에게 책을 소개해주세요.</div>
             </div>
           </div>
           <img className="line-division" alt="" src="vector/line-book.svg" />
