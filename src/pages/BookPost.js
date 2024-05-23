4091cee338dfc1529ffa38e71a22fb2395858bc4
g0yujin <dbwbsqhd016@gmail.com>
2024-05-23 21:23:24 +0900

    DESIGN : Enable textareain BookPost.js

--- src/pages/BookPost.js
+++ src/pages/BookPost.js
@@ -55,7 +55,7 @@ const BookPost = () => {
             <div className="rect-bookname" />
           </div>
           <div className="div-review">
-              <div className="rect-review"/>
+              <textarea className="rect-review" type="text" placeholder="" />
               <div className="subtitle-review">독서 후기</div>
           </div>
           <div className="div-searchbook-btn">
@@ -87,12 +87,11 @@ const BookPost = () => {
           <div className="div-one-line">
             <div className="one-line-notice">50자 이내</div>
             <div className="overlap-4">
-                <div className="rect-one-line" />
+                <textarea className="rect-one-line" type="text" placeholder="다른 사람에게 책을 소개해주세요" />
                 <p className="title-one-line">
                   <span className="subtitle">한줄평</span>
                   <span className="star">*</span>
                 </p>
-              <div className="one-line-notice-2">다른 사람에게 책을 소개해주세요.</div>
             </div>
           </div>
           <img className="line-division" alt="" src="vector/line-book.svg" />
