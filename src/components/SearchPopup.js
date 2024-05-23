45928c1d0ae57efbaed2cba4e4b31bf641309274
g0yujin <dbwbsqhd016@gmail.com>
2024-05-24 00:12:53 +0900

    FEAT:Search-pop implementation

--- src/components/SearchPopup.js
+++ src/components/SearchPopup.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import '../styles/SearchPopup.css'; // Screen 컴포넌트의 스타일을 포함합니다.
 
-const SearchPopup = () => {
-  
-
-    return (
-        <div className="screen">
-          <div className="group">
-            <div className="overlap-group">
-              <img className="line" alt="Line" src="line-25.svg" />
-              <img className="rectangle" alt="Rectangle" src="rectangle-71.png" />
-              <img className="image" alt="Image" src="image-15.png" />
-              <img className="img" alt="Line" src="line-26.svg" />
-              <div className="text-wrapper">불변</div>
-              <div className="div">
-                <img className="img-2" alt="Image" src="image-16.png" />
-                <div className="group-2">
-                  <div className="text-wrapper-2">마케팅 불변의 법칙</div>
-                  <div className="text-wrapper-3">알 리스,잭 트라우트</div>
-                  <div className="text-wrapper-4">비즈니스맵</div>
-                </div>
-              </div>
-              <div className="group-3">
-                <div className="group-4">
-                  <div className="text-wrapper-5">불변의 법칙</div>
-                  <div className="text-wrapper-6">모건 하우절</div>
-                  <div className="text-wrapper-7">서삼독</div>
-                </div>
-                <img className="img-2" alt="Rectangle" src="rectangle-43.png" />
-              </div>
-            </div>
+const SearchPopup = ({ onClose }) => {
+  return (
+    <div className="div-search-popup">
+      <div className="group">
+        <div className="div-search">
+          <img className="line-div-search" alt="" src="/vector/line-div-search-popup.svg" />
+          <img className="btn-close" alt=" " src="/images/btn-close-popup.png" onClick={onClose} />
+          <img className="btn-search" alt=" " src="/images/btn-search-popup.png" />
+          <input className="search-input" type="text" placeholder="검색어를 입력하세요" />  
+        </div>
+        <div className="div-search-result">
+        <div className="group-2">
+          <img className="img-2" alt=" " src="image-16.png" />
+          <div className="group-3">
+            <div className="text-wrapper-2">마케팅 불변의 법칙</div>
+            <div className="text-wrapper-3">알 리스,잭 트라우트</div>
+            <div className="text-wrapper-4">비즈니스맵</div>
           </div>
         </div>
-      );
-    };
+
+        <img className="line-div-search-book" alt="Line" src="/vector/line-search-popup.svg" />
+
+        <div className="group-4">
+          <div className="group-5">
+            <div className="text-wrapper-5">불변의 법칙</div>
+            <div className="text-wrapper-6">모건 하우절</div>
+            <div className="text-wrapper-7">서삼독</div>
+          </div>
+          <img className="img-2" alt="" src="rectangle-43.png" />
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
 
 export default SearchPopup;
