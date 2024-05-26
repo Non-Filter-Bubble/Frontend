144a64e1b9fadd19873199952ec5eb465bb81b41
seoyeon2001 <syeon1250@naver.com>
2024-05-26 19:26:59 +0900

    FIX: axios proxy2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  const target = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080'
-    : 'http://43.203.38.124:8080';
+// module.exports = function(app) {
+//   const target = process.env.NODE_ENV === 'development'
+//     ? 'http://localhost:8080'
+//     : 'http://43.203.38.124:8080';
 
-  app.use(
-    createProxyMiddleware({
-      target: target,
-    //   changeOrigin: true,
-    //   pathRewrite: { '^/api': '' },
-    })
-  );
-};
+//   app.use(
+//     createProxyMiddleware({
+//       target: target,
+//     //   changeOrigin: true,
+//     //   pathRewrite: { '^/api': '' },
+//     })
+//   );
+// };
