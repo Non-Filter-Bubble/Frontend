9cb52d1e793cfbccbbafd3d606f548215fb4f5cd
g0yujin <dbwbsqhd016@gmail.com>
2024-05-26 20:41:06 +0900

    FEAT : icon-heart in search.js

---
+++ src/pages/BookInfo.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import '../styles/BookInfo.css'; 
+import axios from 'axios';
+
+const BookInfo = () => {
+   
+
+    return (
+        <div></div>
+      );
+    };
+export default BookInfo;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,51 +1,64 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
 import '../styles/Search.css'; 
-import axios from 'axios';
 
 const Search = () => {
-   
+  const [books, setBooks] = useState([
+    {
+      title: "불변의 법칙",
+      author: "모건 하우절",
+      company: "서삼독",
+      isFavorite: false,
+      imgSrc: ""
+    },
+    {
+      title: "마케팅 불변의 법칙",
+      author: "알 리스, 잭 트라우트",
+      company: "비즈니스맵",
+      isFavorite: false,
+      imgSrc: ""
+    }
+  ]);
 
-    return (
-        <div className="div-search">
-          <p className="title">‘불변의 법칙'에 대한 2건의 검색 결과</p>
-            <div className="div-search-box">
-            <div className="search-box" />
-            <div className="div-btn-search">
-              <div className="btn-search">
-                <div className="search">검색</div>
-              </div>
-            </div>
-          </div>
-          <img className="line-search" alt="Line" src="/vector/line-search.svg" />
-          <div className="book-1">
-            <img className="img-book-1" alt="" src=""/>
-            <div className="book-1-info">
-                <div className="book-title">불변의 법칙</div>
-             
-              <div className="book-author">모건 하우절</div>
-              <div className="book-company">서삼독</div>
-            </div>
-            <div className="book-service">
-              <img className="icon-heart" alt="" src="/images/empty-heart-search.png" />
-              <img className="icon-cart" alt="" src="/images/icon-cart.png" />
-            </div>
-            <img className="line-book" alt=" " src="/vector/line-search-division.svg" />
-          </div>
-          <div className="book-2">
-            <img className="img-book-1" alt="" src=""/>
-            <div className="book-2-info">
-                <div className="book-title">마케팅 불변의 법칙</div>
-              <div className="book-author">알 리스, 잭 트라우트</div>
-              <div className="book-company">비즈니스맵</div>
-            </div>
-            <div className="book-service">
-              <img className="icon-heart" alt="" src="/images/empty-heart-search.png" />
-              <img className="icon-cart" alt="" src="/images/icon-cart.png" />
-            </div>
-            <img className="line-book" alt=" " src="/vector/line-search-division.svg" />
+  const toggleFavorite = (index) => {
+    const newBooks = [...books];
+    newBooks[index].isFavorite = !newBooks[index].isFavorite;
+    setBooks(newBooks);
+  };
+
+  return (
+    <div className="div-search">
+      <p className="title">‘불변의 법칙'에 대한 2건의 검색 결과</p>
+      <div className="div-search-box">
+        <div className="search-box" />
+        <div className="div-btn-search">
+          <div className="btn-search">
+            <div className="search">검색</div>
           </div>
         </div>
-      );
-    };
+      </div>
+      <img className="line-search" alt="Line" src="/vector/line-search.svg" />
+      {books.map((book, index) => (
+        <div key={index} className={`book-${index + 1}`}>
+          <img className="img-book-1" alt="" src={book.imgSrc}/>
+          <div className={`book-${index + 1}-info`}>
+            <div className="book-title">{book.title}</div>
+            <div className="book-author">{book.author}</div>
+            <div className="book-company">{book.company}</div>
+          </div>
+          <div className="book-service">
+            <img
+              className="icon-heart"
+              alt=""
+              src={book.isFavorite ? "/images/filled-heart-search.png" : "/images/empty-heart-search.png"}
+              onClick={() => toggleFavorite(index)}
+            />
+            <img className="icon-cart" alt="" src="/images/icon-cart.png" />
+          </div>
+          <img className="line-book" alt=" " src="/vector/line-search-division.svg" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default Search;
