78dbb94b3336bab8720ef7aa6dc3cc8bff606e6f
g0yujin <dbwbsqhd016@gmail.com>
2024-05-27 19:35:52 +0900

    FIX : delete axios in BookInfo.js

--- src/pages/BookInfo.js
+++ src/pages/BookInfo.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import '../styles/BookInfo.css'; 
-import axios from 'axios';
 
 const BookInfo = () => {
 
