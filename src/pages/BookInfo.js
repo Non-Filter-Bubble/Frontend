4217775edc6aab8eaacfa4eb78263c13f3312d83
seoyeon2001 <syeon1250@naver.com>
2024-05-28 20:07:01 +0900

    FIX: search api

--- src/pages/BookInfo.js
+++ src/pages/BookInfo.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useNavigate, useLocation } from 'react-router-dom';
+// import React, { useState } from "react";
+// import { useNavigate, useLocation } from 'react-router-dom';
 import '../styles/BookInfo.css'; 
 
 const BookInfo = () => {
