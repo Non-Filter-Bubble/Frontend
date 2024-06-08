5acf3b9b5db1d4869d1b6e14dd15f3efac13d813
seoyeon2001 <syeon1250@naver.com>
2024-06-09 02:44:47 +0900

    FIX: filter api

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef  } from 'react';
 import { useNavigate, useLocation } from "react-router-dom";
 import axiosInstance from '../api/axios';
 
@@ -20,44 +20,51 @@ const Main = () => {
     console.log('recommendData변수에 저장된 값 확인', recommendData);
 
     const token = localStorage.getItem('token');
+    const initialLoad = useRef(true);
 
     // 사용자가 로그인 했는지 확인
     useEffect(() => {
-        if (!token) {
-            alert('로그인이 필요한 서비스입니다.');
-            navigate('/login');
-        } else {
-            // 장르 선택 여부 확인
-            axiosInstance.get(`${process.env.REACT_APP_DB_HOST}/verify-genre`, {
-                headers: {
-                    'authorization': `${token}`
-                }
-            }).then( async (res) => {
-                // console.log(res);
-                // 여기서 DB에 저장된 추천 도서 불러오기
-                console.log('장르 선택 완료한 사용자야 디비에서 추천 도서 가져올거임');
-                
+        const fetchData = async () => {
+            if (!token) {
+                alert('로그인이 필요한 서비스입니다.');
+                navigate('/login');
+            } else {
                 try {
-                    await axiosInstance.get(`${process.env.REACT_APP_DB_HOST}/user/isbn_list`, {
+                    // 장르 선택 여부 확인
+                    await axiosInstance.get(`${process.env.REACT_APP_DB_HOST}/verify-genre`, {
                         headers: {
-                            'authorization': `${token}`,
-                            'Content-Type': 'application/json',
-                        }}
-                    ).then((res) => {
+                            'authorization': `${token}`
+                        }
+                    });
+                    
+                    try {
+                        // DB에 저장된 추천 도서 불러오기
+                        console.log('장르 선택 완료한 사용자야 디비에서 추천 도서 가져올거임');
+                        const res = await axiosInstance.get(`${process.env.REACT_APP_DB_HOST}/user/isbn_list`, {
+                            headers: {
+                                'authorization': `${token}`,
+                                'Content-Type': 'application/json',
+                            }
+                        });
                         console.log(res.data);
                         setRecommendData(res.data);
-                        console.log('디비에서 가져온 정보를 저장한 후에 확인', recommendData);
-
-                    });
+                        console.log('디비에서 가져온 정보를 저장한 후에 확인', res.data);
+    
+                    } catch (error) {
+                        console.error('실패:', error);
+                    }
                 } catch (error) {
                     console.error('실패:', error);
+                    navigate("/join/booktype");
                 }
+
                 
-            }).catch((err) => {
-                // console.log(err);
-                // 장르 선택 페이지로 이동
-                navigate("/join/booktype");
-            }); 
+            }
+        };
+
+        if (initialLoad.current) {
+            fetchData();
+            initialLoad.current = false;
         }
     }, [token, navigate]);
 
