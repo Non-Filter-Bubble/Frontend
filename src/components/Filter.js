3d4decdb1e2a57d9e6b9850ae10ba5815673247e
g0yujin <dbwbsqhd016@gmail.com>
2024-06-16 05:26:20 +0900

    DESIGN: Fix Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -56,13 +56,8 @@ const Filter = ( {filterrecommend} ) => {
   // 3개 미만인 경우 보여지는 걸 1개, 2개만 보여줄 건지? 아님 3개로 보여주고 빈 칸은 없다고 할건지
   const renderComments = () => {
     return [0, 1, 2].map((index) => (
-      <div key={index} className={`oneline-${index + 1}`}>
-        <div className="oneline-3-back"></div>
-        <div className="oneline-31">
-          <div className="div2">
-            <p className="filter-ment3">{bookcomment ? bookcomment[index] : "아직 다른 사용자가 한줄 평을 작성하지 않았어요!"}</p>
-          </div>
-        </div>
+      <div key={index} className="comment-box">
+        <p className="filter-oneline">{bookcomment ? bookcomment[index] : "아직 다른 사용자가 한줄 평을 작성하지 않았어요!"}</p>
       </div>
     ));
   };
@@ -70,51 +65,25 @@ const Filter = ( {filterrecommend} ) => {
 
   return (
     <div className="filter">
+      <div className="div-filter">
       <div className="filter-ment1">이 책과 함께라면, 독서가 더욱 즐거워질 거예요!</div>
         <p className="filter-ment3">마음을 사로잡을 책을 준비했습니다! 다른 독자들의 한줄평을 통해 이 책의 매력을 미리 엿보세요.</p>
         <p className="filter-ment3">이야기의 깊이와 감동이 여러분을 기다립니다. 여러분의 새로운 독서 여정을 시작해보실래요?</p>
      
-      <img className="filter-background" alt="" src="vector/filter-background.svg" />
+        <div className="filter-background">
+          <div className="filter-left" style={{ backgroundImage: `url(${bookcover})` }}>
+            <div className="filter-book-wrapper">
+              {bookcover && <img className="filter-book-icon" alt="" src={bookcover} />}
+              <img className="btn-reset-icon" alt="" src="images/btn-reset.png" onClick={handleFilterLeftClick} />
+            </div>
+          </div>
 
-      <div className="filter-right">
-        {renderComments()}
-        {/* <div className="oneline-1">
-          <div className="oneline-3-back"></div>
-          <div className="oneline-31">
-            <div className="div2">
-              <p className="filter-ment3">{bookcomment ? bookcomment["0"] : "로딩 중..."}</p>
-            </div>
+          <div className="filter-right">
+            {renderComments()}
           </div>
         </div>
-        <div className="oneline-2">
-          <div className="oneline-3-back"></div>
-          <div className="oneline-31">
-            <div className="div2">
-              <p className="filter-ment3">{bookcomment ? bookcomment["1"] : "로딩 중..."}</p>
-            </div>
-          </div>
-        </div>
-        <div className="oneline-3">
-          <div className="oneline-3-back"></div>
-          <div className="oneline-31">
-            <div className="div2">
-              <p className="filter-ment3">{bookcomment ? bookcomment["2"] : "로딩 중..."}</p>
-            </div>
-          </div>
-        </div> */}
-      </div>
-
-      
-      <div className="filterleft" id="filterLeftContainer" >
-        <div className="filter-left">
-          {/* <img className="filter-back-icon" alt="" src="Filter_back.svg" />
-          <img className="filter-back-icon" alt="" src="images/filter-blur.svg" /> */}
-          <div className="filter-book-wrapper">
-            {bookcover && <img className="filter-book-icon" alt="" src={bookcover} />}
-          </div>
-        </div>
-        {/* 다시 버튼을 불렀을 때 */}
-        <img className="btn-reset-icon" alt="" src="images/btn-reset.png" onClick={handleFilterLeftClick} />
+        
+        
       </div>
     </div>
   );
