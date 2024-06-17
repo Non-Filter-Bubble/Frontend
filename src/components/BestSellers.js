8109d4e792f23c3e8d3150234ee0370578aa29cc
g0yujin <dbwbsqhd016@gmail.com>
2024-06-17 18:20:28 +0900

    DESIGN: Complete  Bestsellers

--- src/components/BestSellers.js
+++ src/components/BestSellers.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../styles/BestSellers.css";
 import axiosInstance from "../api/axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Row, Col, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const BestSellers = () => {
     const token = localStorage.getItem('token');
@@ -29,8 +29,9 @@ const BestSellers = () => {
             console.log(`${selectedGenre} 베스트 셀러 목록:`, response.data);
 
             // 8개만 저장
-            const bestSellersWithGenre = response.data.slice(0, 8).map(item => ({
+            const bestSellersWithGenre = response.data.slice(0, 8).map((item, index) => ({
                 ...item,
+                rank: index + 1,
                 GENRE_LV1: selectedGenre
             }));
             setBestSellers(bestSellersWithGenre);
@@ -41,36 +42,38 @@ const BestSellers = () => {
 
     return (
         <div className="div-bestsellers">
-            <h2 className="title">'{genre}' 베스트 셀러 목록</h2>
-          <div className="bestsellers-container">
-          <div className="genre-buttons">
-            <Button variant="outline-dark" className={genre === '소설' ? 'selected' : ''} onClick={() => setGenre('소설')}>소설</Button>
-            <Button variant="outline-dark" className={genre === '자연과학' ? 'selected' : ''} onClick={() => setGenre('자연과학')}>자연과학</Button>
-            <Button variant="outline-dark" className={genre === '인문' ? 'selected' : ''} onClick={() => setGenre('인문')}>인문</Button>
-            <Button variant="outline-dark" className={genre === '자기계발' ? 'selected' : ''} onClick={() => setGenre('자기계발')}>자기계발</Button>
-            <Button variant="outline-dark" className={genre === '경제/경영' ? 'selected' : ''} onClick={() => setGenre('경제/경영')}>경제/경영</Button>
-            <Button variant="outline-dark" className={genre === '시/에세이' ? 'selected' : ''} onClick={() => setGenre('시/에세이')}>시/에세이</Button>
-         </div>
-    
-            <div className="bestsellers-list-container">
-              {bestSellers.map((book, index) => (
-                <Row key={index} className="book-item">
-                  <Col xs={12} md={3} className="book-cover-container">
-                    <img src={book.cover} alt="book_cover" className="book-cover" />
-                  </Col>
-                  <Col xs={12} md={5} className="book-details-container">
-                    <div className="book-details">
-                      <p>{book.title}</p>
-                      <div>저자: {book.author}</div>
-                      <div>출판사: {book.publisher}</div>
-                    </div>
-                  </Col>
-                </Row>
-              ))}
+            <p className="title">'{genre}' 베스트 셀러 </p>
+            
+                <div className="genre-buttons">
+                    <Button variant="outline-dark" className={genre === '소설' ? 'selected' : ''} onClick={() => setGenre('소설')}>소설</Button>
+                    <Button variant="outline-dark" className={genre === '자연과학' ? 'selected' : ''} onClick={() => setGenre('자연과학')}>자연과학</Button>
+                    <Button variant="outline-dark" className={genre === '인문' ? 'selected' : ''} onClick={() => setGenre('인문')}>인문</Button>
+                    <Button variant="outline-dark" className={genre === '자기계발' ? 'selected' : ''} onClick={() => setGenre('자기계발')}>자기계발</Button>
+                    <Button variant="outline-dark" className={genre === '경제/경영' ? 'selected' : ''} onClick={() => setGenre('경제/경영')}>경제/경영</Button>
+                    <Button variant="outline-dark" className={genre === '시/에세이' ? 'selected' : ''} onClick={() => setGenre('시/에세이')}>시/에세이</Button>
+                </div>
+            <div className="bestsellers-container">
+                <div className="bestsellers-list-container">
+                    {bestSellers.map((book, index) => (
+                        <div key={index} className="book-item">
+                            <div className="book-rank">{book.rank}</div>
+                            <div className="book-cover-container">
+                                <img src={book.cover} alt="book_cover" className="book-cover" />
+                            </div>
+                            <div className="book-details-container">
+                                <div className="book-details">
+                                    <p className="title">{book.title}</p>
+                                    <div className="author">저자: {book.author}</div>
+                                    <div className="company">출판사: {book.publisher}</div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <p className="info">도서 DB 제공 : 알라딘 인터넷 서점(www.aladin.co.kr)</p>
             </div>
-            <p>도서 DB 제공 : 알라딘 인터넷 서점(www.aladin.co.kr)</p>
-          </div>
         </div>
-      );
-    };
-    export default BestSellers;
+    );
+};
+
+export default BestSellers;
