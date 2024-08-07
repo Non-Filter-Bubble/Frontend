cc4f0a9b6af2e1ac21e928c23f54c780a9060f5f
g0yujin <dbwbsqhd016@gmail.com>
2024-08-07 21:25:38 +0900

    FEAT: useNavigate in StickyNavbar

--- src/components/StickyNavbar.js
+++ src/components/StickyNavbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import '../styles/StickyNavbar.css'; 
 import { IoHome } from "react-icons/io5";
 import { FaCircleUser } from "react-icons/fa6";
@@ -9,18 +10,44 @@ import { IoMdHelpCircleOutline } from "react-icons/io";
 
 
 
-
-
 const StickyNavbar = () => {
+
+
+  const navigate = useNavigate();
+
+  const userClick = () => {
+    navigate("/user");
+  }
+
+  const homeClick = () => {
+    navigate("/");
+  }
+
+  const searchClick = () => {
+    navigate("/search");
+  }
+
+  const heartClick = () => {
+    navigate("/user?popup=true");
+  }
+
+  const postClick  = () => {
+    navigate("/bookpost");
+  }
+
+  const helpClick  = () => {
+  }
+
+
   return (
     <nav className="sticky-navbar">
       <ul>
-        <li><a href="#mypage"><FaCircleUser size="22"/><span>Home</span></a></li>
-        <li><a href="#home"><IoHome /><span>Home</span></a></li>
-        <li><a href="#search"><IoSearch size="22"/><span>Search</span></a></li>
-        <li><a href="#heart"><GoHeart strokeWidth= '1px' /><span>Favorite</span></a></li>
-        <li><a href="#bookPost"><PiNotePencilBold /><span>Post</span></a></li>
-        <li><a href="#bookPost"><IoMdHelpCircleOutline size="22"/><span>Help</span></a></li>
+        <li onClick={userClick}><a href="#mypage"><FaCircleUser size="22"/><span>Home</span></a></li>
+        <li onClick={homeClick}><a href="#home"><IoHome /><span>Home</span></a></li>
+        <li onClick={searchClick}><a href="#search"><IoSearch size="22"/><span>Search</span></a></li>
+        <li onClick={heartClick}><a href="#heart"><GoHeart strokeWidth= '1px' /><span>Favorite</span></a></li>
+        <li onClick={postClick}><a href="#bookPost"><PiNotePencilBold /><span>Post</span></a></li>
+        <li onClick={helpClick}><a href="#bookPost"><IoMdHelpCircleOutline size="22"/><span>Help</span></a></li>
       </ul>
     </nav>
   );
