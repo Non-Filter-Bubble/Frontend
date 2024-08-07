b9bb46fc860b6ffdbfe800f62901d6fba1f874c4
g0yujin <dbwbsqhd016@gmail.com>
2024-08-07 20:11:24 +0900

    DESIGN : Add react icon

--- src/components/StickyNavbar.js
+++ src/components/StickyNavbar.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import '../styles/StickyNavbar.css'; 
+import { IoHome } from "react-icons/io5";
+import { FaCircleUser } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5";
+import { GoHeart } from "react-icons/go";
+import { PiNotePencilBold } from "react-icons/pi";
+import { IoMdHelpCircleOutline } from "react-icons/io";
+
+
+
+
 
 const StickyNavbar = () => {
   return (
     <nav className="sticky-navbar">
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#mypage"><FaCircleUser size="22"/></a></li>
+        <li><a href="#home"><IoHome /></a></li>
+        <li><a href="#search"><IoSearch size="22"/></a></li>
+        <li><a href="#heart"><GoHeart strokeWidth= '1px' /></a></li>
+        <li><a href="#bookPost"><PiNotePencilBold /></a></li>
+        <li><a href="#bookPost"><IoMdHelpCircleOutline size="22"/></a></li>
       </ul>
     </nav>
   );
