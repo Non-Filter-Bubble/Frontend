dcf9e460a8436868aa1b401952bbd99ec22a9484
g0yujin <dbwbsqhd016@gmail.com>
2024-08-12 21:33:52 +0900

    DESIGN: MainHeader.js

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -6,55 +6,33 @@ import "../styles/MainHeader.css";
 const MainHeader = () => {
   const navigate = useNavigate();
 
-  const searchBookClick = () => {
-    console.log('도서 검색 버튼 클릭');
-    navigate("/search");
+  const nonfilerClick = () => {
+    navigate("/");
   }
 
-  const bookDrawerClick = () => {
-    console.log('북서랍 클릭');
-    navigate("/user");
+  const filerClick = () => {
+    navigate("/");
   }
 
-  const heartClick = () => {
-    console.log('찜 목록 클릭');
+  const bestClick = () => {
     navigate("/user?popup=true");
   }
 
-  const mypageClick = () => {
-    console.log('마이페이지 버튼 클릭');
+  const drawerClick = () => {
     navigate("/user");
   }
 
   return (
-    <div className="main-header">
-      <div className="button-container">
-        <div className="button-div" onClick={searchBookClick}>
-          <div className="inner">
-            <div className="btn-name">도서 검색</div>
-            <img className="icon" alt="" src="images/search-icon.png" />
-          </div>
-        </div>
-        <div className="button-div" onClick={bookDrawerClick}>
-          <div className="inner">
-              <div className="btn-name">북서랍</div>
-              <img className="icon" alt="" src="images/drawer-icon.png" />
-          </div>
-        </div>
-        <div className="button-div" onClick={heartClick}>
-          <div className="inner">
-              <div className="btn-name">찜목록</div>
-            <img className="icon" alt="" src="images/heart.png" />
-          </div>
-        </div>
-        <div className="button-div" onClick={mypageClick}>
-          <div className="inner">
-              <div className="btn-name">마이페이지</div>
-            <img className="icon" alt="" src="images/person.png" />
-          </div>
-        </div>
-      </div>
-    </div>
+    <header className="main-header">
+        <nav>
+            <ul className="nav-list">
+                <li onClick={nonfilerClick} className="list-link">NonFilter</li>
+                <li onClick={filerClick} className="list-link">Filter</li>
+                <li onClick={bestClick} className="list-link">BestSeller</li>
+                <li onClick={drawerClick} className="list-link">BookDrawer</li>
+            </ul>
+        </nav>
+    </header>
   );
 };
   export default MainHeader;
