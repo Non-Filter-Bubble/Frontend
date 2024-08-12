b68f9960cd7a7c1b0efd459049bb649f23952641
g0yujin <dbwbsqhd016@gmail.com>
2024-08-12 18:08:01 +0900

    FEAT: slide-2 in Tutorial.js

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -18,26 +18,22 @@ const Tutorial = ({ onClose }) => {
   return (
     <div className="tutorial-overlay">
       <div className="tutorial-content">
+     {/*슬라이드*/}
         <Slider {...settings}>
 
           <div className="slide-1">
             <p className="ment-big">BUBBLE POP은 <br />
-            여러분의 독서 세계를 넓혀드립니다</p>
+                                    여러분의 독서 세계를 넓혀드립니다</p>
             <p className="ment-small">평소 읽지 않는 새로운 장르의 책을 발견해보세요. <br />
-            호기심을 자극하는 한줄평을 클릭하면 그 숨겨진 이야기의 세계가 펼쳐집니다.</p>
+                                      호기심을 자극하는 한줄평을 클릭하면 그 숨겨진 이야기의 세계가 펼쳐집니다.</p>
           </div>
 
 
           <div className="slide-2">
-            <h1>취향을 넓힐 시간, 이 책을 추천합니다!</h1>
-            <p>여러분의 독서 취향을 한 단계 넓혀줄 특별한 추천 서비스입니다. 평소 선호하는 장르와는 다른 책들을 선입견 없이 만나볼 수 있도록 도와줍니다.</p>
-            <div className="slide-image-container">
-              <img src="path_to_your_image_1" alt="추천 책 1" />
-              <img src="path_to_your_image_2" alt="추천 책 2" />
-              <img src="path_to_your_image_3" alt="추천 책 3" />
-              <img src="path_to_your_image_4" alt="추천 책 4" />
-              <img src="path_to_your_image_5" alt="추천 책 5" />
-            </div>
+            <p className="ment-big">취향을 넓힐 시간, 이 책을 추천합니다!</p>
+            <p className="ment-small">여러분의 독서 취향을 한 단계 넓혀줄 특별한 추천 서비스입니다. <br />
+                                      평소 선호하는 장르와는 다른 책들을 선입견 없이 만나볼 수 있도록 도와줍니다.</p>
+            <img className="nonfilter-img" alt ="" src="/images/tutorial-nonfilter.png"/>
           </div>
 
           <div className="slide-3">
