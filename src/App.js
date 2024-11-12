772b8175d592704c8bf7d1d8c89e8e865fe6bd4c
g0yujin <dbwbsqhd016@gmail.com>
2024-11-12 17:57:57 +0900

    FIX: StickyNavbar's token

--- src/App.js
+++ src/App.js
@@ -116,7 +116,8 @@ function App() {
     <div className='App'>
       <div className='header'>
         <Header />
-        {token && <StickyNavbar />} {/* 로그인된 경우에만 StickyNavbar 렌더링 */}
+        {token && location.pathname !== '/join/booktype' && location.pathname !== '/join/genre' && <StickyNavbar />} 
+        {/* 로그인된 경우이고, SelectBookType과 SelectGenre 페이지가 아닌 경우에만 StickyNavbar 렌더링 */}
       </div>
       <div className="content-container">
         <Routes>
